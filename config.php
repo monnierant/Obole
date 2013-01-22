0e69c37fbbf9162260b35d3b54de82ac6fb5c3eb
bibehck <hiob.bibehck@gmail.com>
2013-01-22 04:08:03 -0800

    Create config.php
    config.php file to run with ipn.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$DB_Server = ""; // MySQL host.
+$DB_Username = "; // MySQL username.
+$DB_Password = ""; // MySQL password.
+$DB_DBName = ""; // MySQL database name.
+$DBsandbox = false;
+
+$notify_email = "";
+$currency = "US";
+
+?>
